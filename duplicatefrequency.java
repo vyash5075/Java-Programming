66fbc45269e9f3bb076d9302fa696d6ac2664021
vyash5075 <vyash5075@gmail.com>
2019-12-08 08:07:01 +0530

    count duplicates frequency  in array

---
+++ duplicatefrequency.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class duplicatefrequency {
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the no. of entries");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    int arr2[]=new int[1000];
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++)
+    {
+        arr2[arr[i]]=arr2[arr[i]]+1;
+    }
+    System.out.println("frequency of duplicate elements are");
+    for(int i=0;i<n;i++)
+    {
+        if(arr2[arr[i]]>1)
+        {
+            System.out.println("duplicate no. is "+arr[i]+"frequency is "+arr2[arr[i]]);
+            arr2[arr[i]]=0;
+        }
+    }
+    
+}
+}
