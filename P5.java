e1a22d7a67d6286ddd0bff8595d6a8cb5c7d08e5
Yash Verma <vyash5075@gmail.com>
2022-02-12 23:28:55 +0530

    Create P5.java

---
+++ P5.java
@@ -0,0 +1,29 @@
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int rows=sc.nextInt();
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<rows-i-1;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<(2*i)+1;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
