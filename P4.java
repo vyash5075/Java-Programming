97eb0d8c1e7b17e34a2a19d760b4e44b93fc63f4
Yash Verma <vyash5075@gmail.com>
2022-02-12 22:57:11 +0530

    Create P4.java

---
+++ P4.java
@@ -0,0 +1,30 @@
+
+   
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of Rows");
+        int rows=sc.nextInt();
+         for(int i=0;i<rows;i++){
+             for(int j=0;j<=i;j++){
+                 System.out.print("  ");
+             }
+             for(int k=i;k<rows;k++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+    }
+}
