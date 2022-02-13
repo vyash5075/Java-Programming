ddd67da48aff8e739b7f0a192dd51987c7355592
Yash Verma <vyash5075@gmail.com>
2022-02-13 13:56:31 +0530

    Create P12.java

---
+++ P12.java
@@ -0,0 +1,35 @@
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no of rows");
+        int rows=sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<rows-i;j++){
+                System.out.print("* ");
+            }
+             
+            System.out.println();
+        }
+        for(int i=1;i<rows;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
