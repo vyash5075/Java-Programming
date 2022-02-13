97877b5308bc33b8026654efa1c756f670d6618d
Yash Verma <vyash5075@gmail.com>
2022-02-13 16:47:19 +0530

    Create P23.java

---
+++ P23.java
@@ -0,0 +1,58 @@
+
+   
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+        * 
+      * * 
+    *   * 
+  *     * 
+*       * 
+  *     * 
+    *   * 
+      * * 
+        * 
+          
+
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int rows=sc.nextInt();
+         
+        int rowse=rows-1;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<rows-i-1;j++){
+                
+                System.out.print("  ");
+            }
+            for(int k=0;k<i+1;k++){
+                if(k==0||k==i){
+                     System.out.print("* ");
+                }
+               else{
+                    System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+        
+        for(int i=0;i<=rowse;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("  ");
+            }
+            for(int j=0;j<=rowse-1-i;j++){
+                if(j==0||j==rows-i-2){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
