b51d79aa7e7da94f9c0c34b06631bb2a7fc47705
Yash Verma <vyash5075@gmail.com>
2022-02-13 16:38:41 +0530

    Create P27.java

---
+++ P27.java
@@ -0,0 +1,59 @@
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+* * * * *   * * * * * 
+* * * *       * * * * 
+* * *           * * * 
+* *               * * 
+*                   * 
+*                   * 
+* *               * * 
+* * *           * * * 
+* * * *       * * * * 
+* * * * *   * * * * * 
+
+  
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+         
+        for(int i=0;i<rows;i++){
+             for(int j=i;j<rows;j++){
+                 System.out.print("* ");
+             }
+             for(int k=0;k<i;k++){
+                 System.out.print("  ");
+             }
+              for(int j=0;j<=i;j++){
+                 System.out.print("  ");
+             }
+             for(int k=i;k<rows;k++){
+                 System.out.print("* ");
+             }
+         
+            System.out.println();
+        }
+        
+        for(int i=0;i<rows;i++){
+             for(int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+            for(int k=i;k<rows;k++){
+               System.out.print("  ");
+            }
+              for(int k=rows-1;k>i;k--){
+                System.out.print("  ");
+            }
+            for(int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+             
+            
+            System.out.println();
+        }
+          
+    }
+}
