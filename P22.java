42b5d1a1bd170e9a4f6aa3e44fa7bc5df9d6162b
Yash Verma <vyash5075@gmail.com>
2022-02-13 16:44:15 +0530

    Create P22.java

---
+++ P22.java
@@ -0,0 +1,45 @@
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+* 
+* * 
+*   * 
+*     * 
+*       * 
+*     * 
+*   * 
+* * 
+* 
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int rows=sc.nextInt();
+         for(int i=0;i<rows;i++){
+            for(int j=0;j<i+1;j++){
+                if(j==0||j==i){
+                  System.out.print("* ");   
+                }
+               else{
+                    System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+        
+        for(int i=0;i<rows-1;i++){
+            for(int j=0;j<rows-i-1;j++){
+                if(j==0||j==rows-i-2){
+                     System.out.print("* ");
+                }
+                else{
+                     System.out.print("  ");
+                }
+               
+            }
+            System.out.println();
+        }
+    }
+}
