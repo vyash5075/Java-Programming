41635cd5c70be5d98f2c13469ee3a7298b73c336
Yash Verma <vyash5075@gmail.com>
2022-02-13 17:22:18 +0530

    Create P30.java

---
+++ P30.java
@@ -0,0 +1,28 @@
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int rows=sc.nextInt();
+         
+        
+          for(int i=0;i<rows;i++){
+              for(int j=0;j<rows;j++){
+                  System.out.print("* ");
+              }
+              System.out.println();
+          }
+             
+         }
+    
+}
