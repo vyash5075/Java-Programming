2d8655541acb396ef3622701b2eddac7eba352db
Yash Verma <vyash5075@gmail.com>
2022-02-13 17:18:11 +0530

    Create P29.java

---
+++ P29.java
@@ -0,0 +1,41 @@
+
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows");
+        int rows=sc.nextInt();
+         
+        int rowse=rows-1;
+         for(int i=0;i<rows;i++){
+             for(int j=0;j<i;j++){
+                 System.out.print("  ");
+             }
+             for(int k=0;k<(2*(rows-i)-1);k++){
+                 if(k==0||i==0||k==(2*(rows-i)-2)){
+                     System.out.print("* ");
+                 }
+                 else{
+                     System.out.print("  ");
+                 }
+                 
+             }
+             System.out.println();
+         }
+         for(int i=1;i<rows;i++){
+             for(int j=0;j<rows-i-1;j++){
+                 System.out.print("  ");
+             }
+             for(int k=0;k<2*i+1;k++){
+                   if(k==0||i==rows-1||k==2*i){
+                     System.out.print("* ");
+                 }
+                 else{
+                     System.out.print("  ");
+                 }
+             }
+             System.out.println();
+         }
+    }
+}
