bc3a2cd8857b1400b6a66f6ed234c9936c97c89c
Yash Verma <vyash5075@gmail.com>
2022-02-13 15:53:04 +0530

    Create P20.java

---
+++ P20.java
@@ -0,0 +1,35 @@
+
+   
+## Input
+Enter the number of rows needed to print the pattern 
+5
+## output
+
+* * * * * * * * * 
+  *           * 
+    *       * 
+      *   * 
+        * 
+        
+        
+        import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+         for(int i=0;i<rows;i++){
+             for(int j=0;j<i;j++){
+                 System.out.print("  ");
+             }
+             for(int k=i;k<2*rows-i-1;k++){
+                 if(k==i||i==0||k==2*rows-i-2){
+                      System.out.print("* ");
+                 }
+                else{
+                     System.out.print("  ");
+                }
+             }
+             System.out.println();
+         }
+    }
+}
