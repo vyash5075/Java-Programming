92893d3e7ae1f51ed695db17b7b9900b5f58e3c2
Yash Verma <vyash5075@gmail.com>
2022-02-16 01:24:25 +0530

    Create benjaminbulbs.java

---
+++ benjaminbulbs.java
@@ -0,0 +1,14 @@
+##input
+49
+##output
+4,9,1,25,36,49
+import java.util.*;
+public class benjaminbulbs{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        for(int i=0;i*i<=n1;i++){
+            System.out.print(i*i+",");
+        }
+    }
+}
