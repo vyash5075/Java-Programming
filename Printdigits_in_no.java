3cc061a30bdee47a446863947bc2974173194146
Yash Verma <vyash5075@gmail.com>
2022-02-15 16:23:03 +0530

    Create Printdigits_in_no.java

---
+++ Printdigits_in_no.java
@@ -0,0 +1,30 @@
+##input
+987
+##output
+9
+8
+7
+import java.util.*;
+public class printno{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no of fibonacci no you want to print");
+        int n=sc.nextInt();
+        int k=n;
+       int count=0;
+       while(k!=0){
+           count++;
+           k=k/10;
+       }
+       int div=(int)Math.pow(10,count-1);
+       while(n!=0){
+           int result=n/div;
+           System.out.println(result);
+           n=n%div;
+           div=div/10;
+           
+       }
+       
+       
+    }
+}
