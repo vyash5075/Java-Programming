ae27b67644f50f2cfb3e38f1235fe32f7d4e3a39
Yash Verma <vyash5075@gmail.com>
2022-02-15 16:12:20 +0530

    Create countDigits_in_no.java

---
+++ countDigits_in_no.java
@@ -0,0 +1,18 @@
+##input
+1000
+#output
+4
+import java.util.*;
+public class fibonacci{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no of fibonacci no you want to print");
+        int n=sc.nextInt();
+       int count=0;
+       while(n!=0){
+           count++;
+           n=n/10;
+       }
+       System.out.print(count);
+    }
+}
