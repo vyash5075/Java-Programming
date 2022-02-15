e5006dfad57cb6f9fda252fb395f6b2b43029cb3
Yash Verma <vyash5075@gmail.com>
2022-02-15 15:36:46 +0530

    Create Primenocheck.java

---
+++ Primenocheck.java
@@ -0,0 +1,24 @@
+##input 
+5
+##output
+not prime
+
+import java.util.*;
+public class primecheck{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=2;i*i<n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count>0){
+            System.out.println("not prime");
+        }
+        else{
+            System.out.println("Prime");
+        }
+    }
+}
