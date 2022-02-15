c2e05d505097bbd998d911d060ea8f4bc613fa7d
Yash Verma <vyash5075@gmail.com>
2022-02-15 16:27:54 +0530

    Update countDigits_in_no.java

--- countDigits_in_no.java
+++ countDigits_in_no.java
@@ -3,7 +3,7 @@
 #output
 4
 import java.util.*;
-public class fibonacci{
+public class countdigits{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the no of fibonacci no you want to print");
