b59855edbbdd4ea358208dff7a3e7b8ea5c13d84
Yash Verma <vyash5075@gmail.com>
2022-02-15 15:49:01 +0530

    Create Prime_no_btw_range.java

---
+++ Prime_no_btw_range.java
@@ -0,0 +1,27 @@
+##input
+10
+20
+##output
+11,13,17,19
+  import java.util.*;
+public class primecheck{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+         int low=sc.nextInt();
+         int high=sc.nextInt();
+         for(  int no=low;no<high;no++){
+             int count=0;
+             
+             for(int i=2;i*i<no;i++){
+                 if(no%i==0){
+                     count++;
+                     break;
+                 }
+                 else{}
+             }
+             if(count==0){
+                  System.out.println(no+",");
+             }
+         }
+    }
+}
