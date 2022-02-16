88b16dbf2fbdab09d27358e20ed9c65a77b283c0
Yash Verma <vyash5075@gmail.com>
2022-02-16 18:17:38 +0530

    ☄️📌

---
+++ P43.java
@@ -0,0 +1,26 @@
+0	
+1	1	
+2	3	5	
+8	13	21	34	
+55	89	144	233	377	
+
+ import java.util.*;
+public class numberpattern{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a=0;
+    int b=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(a+"\t");
+            int c=a+b;
+            a=b;
+            b=c;
+        
+            
+        }
+        System.out.println();
+    }
+}
+}
