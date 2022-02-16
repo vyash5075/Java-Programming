eca37bcd6625c953d1a907a2ea66865ecd9b1c3f
Yash Verma <vyash5075@gmail.com>
2022-02-17 00:32:02 +0530

    Create P48.java

---
+++ P48.java
@@ -0,0 +1,38 @@
+		*	
+		*	*	
+*	*	*	*	*	
+		*	*	
+		*	
+
+      
+import java.util.*;
+public class ABC{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sp=n/2;
+        int  st=1;
+        for(int i=1;i<=n;i++){
+            
+           for(int j=1;j<=sp;j++){
+               
+           if(i==n/2+1){ System.out.print("*\t");}
+           else{
+                System.out.print("\t");
+           }
+               
+            }
+            
+            for(int j=1;j<=st;j++){
+                System.out.print("*\t");
+            }
+            if(i<=n/2){
+                st++;
+            }
+            else{
+                st--;
+            }
+            System.out.println();
+        }
+    }
+}
