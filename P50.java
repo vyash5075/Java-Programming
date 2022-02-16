e034237e94c890f978ab902a73f693b3c5e4dc32
Yash Verma <vyash5075@gmail.com>
2022-02-17 00:55:44 +0530

    Create P50.java

---
+++ P50.java
@@ -0,0 +1,42 @@
+*	*	*	*	*	*	*	
+	*				*	
+		*		*	
+			*	
+		*	*	*	
+	*	*	*	*	*	
+*	*	*	*	*	*	*	
+
+
+import java.util.*;
+public class pattern{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int st=n;
+         int sp=0;
+         for(int i=1;i<=n;i++){
+             for(int j=1;j<=sp;j++){
+                 System.out.print("\t");
+             }
+             for(int j=1;j<=st;j++){
+                 if(i>1&&i<=n/2&&j>1&&j<st){
+                     System.out.print("\t");
+                 }
+                 else{
+                     
+                     System.out.print("*\t");
+                 }
+             }
+             
+             if(i<=n/2){
+                 sp++;
+                 st-=2;
+             }
+             else{
+                 sp--;
+                 st+=2;
+             }
+             System.out.println();
+         }
+    }
+}
