079ecdb19d606be9a9314d7d710950877af1935d
Yash Verma <vyash5075@gmail.com>
2022-02-16 18:55:19 +0530

    ☄️

---
+++ P44.java
@@ -0,0 +1,25 @@
+1	
+1	1	
+1	2	1	
+1	3	3	1	
+1	4	6	4	1	
+
+  import java.util.*;
+public class numberpattern{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+   
+    for(int i=0;i<n;i++){
+        int icj=1;
+        
+        for(int j=0;j<=i;j++){
+            System.out.print(icj+"\t");
+            int icjp1=icj*(i-j)/(j+1);
+          icj=icjp1;
+            
+        }
+        System.out.println();
+    }
+}
+}
