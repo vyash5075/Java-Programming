35abce38bae604e2088df354a310cbe2a69ceea2
Yash Verma <vyash5075@gmail.com>
2022-02-16 13:31:30 +0530

    Update P32.java

--- P32.java
+++ P32.java
@@ -52,3 +52,37 @@ public class pattern{
          }
     }
 }
+
+
+
+
+// another solution 
+
+*				*	
+	*		*		
+		*			
+	*		*		
+*				*	
+
+ import java.util.*;
+public class rightdiagonal{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print("*\t");
+                }
+                else if(i+j==n+1){
+                     System.out.print("*\t"); 
+                }
+                else{
+                      System.out.print("\t");
+                }
+                 
+            }
+             System.out.println();
+        }
+    }
+}
