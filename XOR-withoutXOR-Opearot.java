e166a3eea992e91b1d2b1c19e70c85075ee63d07
Yash Verma <vyash5075@gmail.com>
2022-02-21 13:34:22 +0530

    Create XOR-withoutXOR-Opearot.java

---
+++ XOR-withoutXOR-Opearot.java
@@ -0,0 +1,46 @@
+
+Input:  x = 1, y = 2
+Output: 3
+
+Input:  x = 3, y = 5
+Output: 6
+
+// Java program to find XOR without using ^
+import java.io.*;
+ 
+class GFG{
+   
+// Returns XOR of x and y
+static int myXOR(int x, int y)
+{
+     
+    // Initialize result
+    int res = 0;
+ 
+    // Assuming 32-bit Integer
+    for(int i = 31; i >= 0; i--)                    
+    {
+         
+        // Find current bits in x and y
+        int b1 = ((x & (1 << i)) == 0 ) ? 0 : 1; 
+        int b2 = ((y & (1 << i)) == 0 ) ? 0 : 1; 
+ 
+        // If both are 1 then 0 else xor is same as OR
+        int xoredBit = ((b1 & b2) != 0) ? 0 : (b1 | b2);         
+ 
+        // Update result
+        res <<= 1;
+        res |= xoredBit;
+    }
+    return res;
+}
+ 
+// Driver Code
+public static void main (String[] args)
+{
+    int x = 3, y = 5;
+     
+    System.out.println("XOR is " + myXOR(x, y));
+}
+}
+ 
