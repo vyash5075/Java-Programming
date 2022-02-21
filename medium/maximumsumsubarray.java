174d6abc4ce39b65f7395b7098ca94f906591a49
Yash Verma <vyash5075@gmail.com>
2022-02-21 19:27:12 +0530

    Update maximumsumsubarray.java

--- medium/maximumsumsubarray.java
+++ medium/maximumsumsubarray.java
@@ -5,17 +5,25 @@ public class maximumsumsubarray{
         System.out.println(maxsubarray(ar1));
     }
     public static int maxsubarray(int ar1[]){
+        int start=0;
+        int end=0;
+        int s=0;
+        
         int max_so_far=Integer.MIN_VALUE;
         int max_ending=0;
         for(int i=0;i<ar1.length;i++){
             max_ending=max_ending+ar1[i];
             if(max_so_far<max_ending){
                 max_so_far=max_ending;
+                start=s;
+                end=i;
             }
             if(max_ending<0){
                 max_ending=0;
+                s=i+1;
             }
         }
+        System.out.println("Starting pos is "+start+" and ending pos is"+end);
         return max_so_far;
     }
 }
