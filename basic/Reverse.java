13e27a683ce86d60388e5faf90cb31e0b4e7fe06
Yash Verma <vyash5075@gmail.com>
2022-02-27 17:42:57 +0530

    🚀

---
+++ basic/Reverse.java
@@ -0,0 +1,32 @@
+##Input 
+Yash verma
+##output
+amrev hsay
+
+case 1:
+import java.util.*;
+public class reverseString{
+    public static void main(String args[]){
+        String s="yash verma";
+        String rev="";
+        for(int j=s.length()-1;j>=0;j--){
+            rev+=s.charAt(j);
+        }
+        System.out.println(rev);
+    }
+}
+
+
+case2: using two pointer approach
+
+import java.util.*;
+public class reverseString{
+    public static void main(String args[]){
+        String s="yash verma";
+        String rev="";
+        for(int j=s.length()-1;j>=0;j--){
+            rev+=s.charAt(j);
+        }
+        System.out.println(rev);
+    }
+}
