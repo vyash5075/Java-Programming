d82d9ad8d09090ed2c76302eff8fe1352c85ace3
Yash Verma <vyash5075@gmail.com>
2022-02-28 11:56:02 +0530

    Create AddTwoStrings.java

---
+++ basic/AddTwoStrings.java
@@ -0,0 +1,17 @@
+#Input
+s1="111";
+s2="222";
+#output
+333
+import java.util.*;
+public class balancedString{
+    public static void main(String args[])
+{
+   String s="111";
+   String s2="999";
+   int a=Integer.parseInt(s);
+    int a1=Integer.parseInt(s2);
+   System.out.println(a+a1);
+ 
+    
+}}
