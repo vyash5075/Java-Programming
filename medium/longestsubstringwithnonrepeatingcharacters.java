fd23691cf4051c3c9c3a40834e463ce2a0612dee
Yash Verma <vyash5075@gmail.com>
2022-03-05 11:22:00 +0530

    Update longestsubstringwithnonrepeatingcharacters.java

--- medium/longestsubstringwithnonrepeatingcharacters.java
+++ medium/longestsubstringwithnonrepeatingcharacters.java
@@ -3,6 +3,7 @@ badccbacd
 ##output
 4bacd
 
+time complexity=0(1)
 import java.util.*;
 public class longestsubstringwithnonrepeatingcharacters{
     public static void main(String args[]){
@@ -38,3 +39,65 @@ public class longestsubstringwithnonrepeatingcharacters{
         return maxlength;
     }
 }
+
+
+
+
+
+//another approach easy asswell
+time complexity=0(1)
+
+
+import java.io.*;
+ 
+public class GFG {
+    public static int longestUniqueSubsttr(String str)
+    {
+        String finals="";
+        String test = "";
+  
+        int maxLength = -1;
+ 
+      
+        if (str.isEmpty()) {
+            return 0;
+        }
+        
+        else if (str.length() == 1) {
+            return 1;
+        }
+        for (char c : str.toCharArray()) {
+            String current = String.valueOf(c);
+ 
+            
+            if (test.contains(current)) {
+                 
+                test = test.substring(test.indexOf(current)
+                                      + 1);
+                                      
+                                      
+            }
+            test = test + String.valueOf(c);
+             
+            if(test.length()>maxLength){
+                 maxLength = Math.max(test.length(), maxLength);
+                 finals=test;
+                 
+            }
+           
+        }
+    System.out.println(finals);
+        return maxLength;
+    }
+
+    public static void main(String[] args)
+    {
+        String str = "geeksforgeeks";
+         
+        int len = longestUniqueSubsttr(str);
+        System.out.println(
+                            
+                             "substring is " + len);
+    }
+}
+ 
