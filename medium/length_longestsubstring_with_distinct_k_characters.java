e0c0928c590c086f4b32cca4eedb0568e58f495e
Yash Verma <vyash5075@gmail.com>
2022-03-05 09:59:06 +0530

    Update length_longestsubstring_with_distinct_k_characters.java

--- medium/length_longestsubstring_with_distinct_k_characters.java
+++ medium/length_longestsubstring_with_distinct_k_characters.java
@@ -2,8 +2,9 @@
 k=2
 s=bccbababd
 ##output
-5
 babab
+5
+
 
 import java.util.*;
 public class longestsubstringwithdistinctkcharacters{
@@ -19,6 +20,9 @@ public class longestsubstringwithdistinctkcharacters{
         int start=0;
         int end=0;
         int maxlength=0;
+        int finalStart=0;
+        int finalEnd=0;
+        
         HashMap<Character,Integer>hm=new HashMap<Character,Integer>();
         for(int i=0;i<str.length();i++){
             char c=str.charAt(i);
@@ -31,10 +35,17 @@ public class longestsubstringwithdistinctkcharacters{
                  }
                  start++;
             }
+            
+            if(end-start+1>maxlength){
+                maxlength=end-start+1;
+                finalStart=start;
+                finalEnd=end;
+            }
            maxlength=Math.max(end-start+1,maxlength);
             
             end++;
         }
+        System.out.println(finalStart+","+finalEnd+ str.substring(finalStart,finalEnd+1));
         return maxlength;
     }
 }
